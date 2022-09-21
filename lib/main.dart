b1b26e48bd9b5f65d7b6bf3c4ac529671bf6dd27
Samuel Frei <mail@sfrei.io>
2022-09-21 11:14:26 +0200

    Set flutter app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class QueensQuest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Queens Quest',
       theme: ThemeData(
         // This is the theme of your application.
         //
